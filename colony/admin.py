0ba4561799ec6a27978a0bd3024bc2ada4015d16
ross128 <ross128@geekmail.de>
2013-09-14 15:43:39 +0200

    adding images to colony views

--- colony/admin.py
+++ colony/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from colony.models import Colony
+from colony.models import *
 
 admin.site.register(Colony)
-
+admin.site.register(Field)
+admin.site.register(FieldAssignment)
--- colony/models.py
+++ colony/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Field(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		"""prints desription"""
+		return self.name
+
 class Colony(models.Model):
 	name = models.CharField(max_length=200)
 	owner = models.ForeignKey(User)
+	fields = models.ManyToManyField(Field, through='FieldAssignment')
 
 	class Meta(object):
 		verbose_name_plural = "Colonies"
@@ -12,4 +20,12 @@ class Colony(models.Model):
 		"""prints description"""
 		return self.name
 
+class FieldAssignment(models.Model):
+	colony = models.ForeignKey(Colony)
+	field = models.ForeignKey(Field)
+	# building = models.ForeignKey(Building)
+
+	def __unicode__(self):
+		"""prints description"""
+		return "%s has %s" % (self.colony.name,self.field.name)
 

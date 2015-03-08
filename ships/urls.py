71b0b967416794b349e87ed409d23fc802995fdb
ross128 <ross128@geekmail.de>
2015-03-08 13:57:55 +0100

    added simple ship movement

--- ships/urls.py
+++ ships/urls.py
@@ -3,11 +3,11 @@ from ships import views
 
 urlpatterns = patterns('ships.views',
 
-   # #field details
-   # url(r'^(?P<colony_id>\d+)/field/(?P<x>\d+)/(?P<y>\d+)/$', views.fielddetail, name='fielddetail'),
+	# ship movement
+	url(r'^(?P<ship_id>\d+)/move/$', views.move, name='move'),
 
-   # ship detail
-   url(r'^(?P<ship_id>\d+)/$', views.ship, name='detail'),
+	# ship detail
+	url(r'^(?P<ship_id>\d+)/$', views.ship, name='detail'),
 
 	# list
 	url(r'^$', views.shiplist, name='list'),
--- ships/views.py
+++ ships/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
 from ships.models import *
 from map.util import get_map
 
@@ -17,4 +20,31 @@ def ship(request, ship_id):
 		'ship': ship,
 		'local_map': local_map
 	})
+
+@login_required
+def move(request, ship_id):
+	"""moves ship"""
+	ship = get_object_or_404(Ship, pk=ship_id)
+
+	try:
+		jump = int(request.POST['jump'])
+		if jump < 1:
+			messages.error(request, "could not move ship")
+		else:
+			if 'up' in request.POST:
+				ship.y -= jump
+				messages.success(request, "moved up: %s" % jump)
+			elif 'left' in request.POST:
+				messages.success(request, "moved left: %s" % jump)
+				ship.x -= jump
+			elif 'right' in request.POST:
+				messages.success(request, "moved right: %s" % jump)
+				ship.x += jump
+			elif 'down' in request.POST:
+				messages.success(request, "moved down: %s" % jump)
+				ship.y += jump
+			ship.save()
+	except Exception as e:
+		messages.error(request, "could not move ship")
+	return HttpResponseRedirect(reverse('ships:detail', args=(ship_id,)))
 

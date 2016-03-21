fb5d5fd9bfb94c355f56b8b2e3513c6ddd08688a
ross128 <ross128@geekmail.de>
2016-03-21 17:34:53 +0100

    added inline admin interfaces for ship and colony

--- colony/admin.py
+++ colony/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from colony.models import *
 
-admin.site.register(Colony)
+class FieldAssignmentInlineAdmin(admin.TabularInline):
+	model = FieldAssignment
+	fields = ('field', 'x', 'y')
+	extra = 1
+
+class ColonyAdmin(admin.ModelAdmin):
+	inlines = (FieldAssignmentInlineAdmin,)
+	list_display = ('name', 'owner')
+	list_filter = ('owner',)
+	list_per_page = 100
+	search_fields = ('name',)
+	fieldsets = [
+		('Colony', {
+			'fields': ['name', 'owner'],
+		}),
+	]
+
+admin.site.register(Colony, ColonyAdmin)
 admin.site.register(Field)
 admin.site.register(FieldAssignment)
 admin.site.register(Building)
--- ships/admin.py
+++ ships/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from ships.models import *
 
+class ShipModuleAssignmentInlineAdmin(admin.TabularInline):
+	model = ModuleAssignment
+	fields = ('module', 'count')
+	extra = 1
+
+class ShipAdmin(admin.ModelAdmin):
+	inlines = (ShipModuleAssignmentInlineAdmin,)
+	list_display = ('name', 'shipclass', 'owner', 'x', 'y')
+	list_filter = ('shipclass', 'owner')
+	list_per_page = 100
+	search_fields = ('name',)
+	fieldsets = [
+		('Ship', {
+			'fields': ['name', 'shipclass', 'owner', 'x', 'y'],
+		}),
+	]
 admin.site.register(ShipClass)
-admin.site.register(Ship)
+admin.site.register(Ship, ShipAdmin)
 admin.site.register(Module)
 admin.site.register(ModuleAssignment)
 admin.site.register(ShipProperty)
